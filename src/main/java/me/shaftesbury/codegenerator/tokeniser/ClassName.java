50630968c1d0c27c9428f43f2382f9f763981326
rpolton <richaerd>
2019-12-28 21:16:46 +0000

    add first cut of tokeniser that recognises 'new' keyword and classname

--- src/main/java/me/shaftesbury/codegenerator/tokeniser/ClassName.java
+++ src/main/java/me/shaftesbury/codegenerator/tokeniser/ClassName.java
@@ -1,6 +1,10 @@
 package me.shaftesbury.codegenerator.tokeniser;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class ClassName implements IToken {
+    private static final ClassName CLASSNAME_AS_TOKEN = new ClassName("");
     private final String name;
 
     private ClassName(final String name) {
@@ -9,5 +13,38 @@ public class ClassName implements IToken {
 
     public static ClassName of(final String name) {
         return new ClassName(name);
+    }
+
+    public static ClassName token() {
+        return CLASSNAME_AS_TOKEN;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassName{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final ClassName className = (ClassName) o;
+
+        if (this == CLASSNAME_AS_TOKEN || className == CLASSNAME_AS_TOKEN)
+            return true;
+        return new EqualsBuilder()
+                .append(name, className.name)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(name)
+                .toHashCode();
     }
 }
--- src/main/java/me/shaftesbury/codegenerator/tokeniser/Tokeniser.java
+++ src/main/java/me/shaftesbury/codegenerator/tokeniser/Tokeniser.java
@@ -16,6 +16,15 @@ public class Tokeniser implements ITokeniser {
             return tokenise(body.replaceFirst("^new +", ""), tokens.prepend(Token.NEW));
         }
 
+        if (tokens.startsWith(List.of(Token.NEW))) {
+            final String className = body.substring(0, body.indexOf("("));
+            return tokenise(body.replaceFirst("^[a-zA-Z0-9_]+ *", ""), tokens.prepend(ClassName.of(className)));
+        }
+
+        if (tokens.startsWith(List.of(ClassName.token())) && body.startsWith("()")) {
+            return tokenise(body.replaceFirst("()", ""), tokens.prepend(Token.NOPARAMS));
+        }
+
         return tokens.reverse();
     }
 }
